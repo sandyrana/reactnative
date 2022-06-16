ae28d97e180bda1615a7741fd2cc9530e916799e
Sandeep Kumar Rana <60137332+sandyrana@users.noreply.github.com>
2022-06-16 20:27:29 +0530

    Update CustomSidebarMenu.js

--- src/navigation/CustomSidebarMenu.js
+++ src/navigation/CustomSidebarMenu.js
@@ -1,64 +1,168 @@
 //This is an example code for Navigation Drawer with Custom Side bar//
-import React, {Component} from 'react';
-import {Image, Platform, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {Ionicons,AntDesign,Entypo  } from '@expo/vector-icons';
-import {getUserDetails, logout} from '../utils/LocalStorage';
+import React, { Component } from 'react';
+import { Image, Platform, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View,Share } from 'react-native';
+import { Ionicons, AntDesign, Entypo,MaterialCommunityIcons,MaterialIcons} from '@expo/vector-icons';
+import { getUserDetails, logout } from '../utils/LocalStorage';
+import { Base_Image,BASE_URL } from '../axios/Api'
+import ToggleSwitch from 'toggle-switch-react-native'
+
+// import Icon from 'react-native-feather1s';
 
 export default class CustomSidebarMenu extends Component {
-    
+
     constructor() {
         super();
         this.state = {
             user: null,
+            fig:false,
+            camerlogin:false,
+            check:0
         };
         //Setting up the Main Top Large Image of the Custom Sidebar
-       
+   
 
         this.proileImage =
             'https://aboutreact.com/wp-content/uploads/2018/07/sample_img.png';
 
+
         this.items = [
             {
-                navOptionThumb: '<Entypo name="home" size={24} color="black" />',
+                navOptionThumb: <Entypo name="home" size={24} color="black" />,
                 navOptionName: 'Home',
                 screenToNavigate: 'Home',
             },
             {
-                navOptionThumb: 'user',
+                navOptionThumb: <Entypo name="user" size={24} color="black" />,
                 navOptionName: 'My Profile',
                 screenToNavigate: 'Profile',
             },
-           
+            {
+									
+                navOptionThumb: <MaterialCommunityIcons name="cart" size={24}  />,               
+                navOptionName: 'Cart',
+                screenToNavigate: 'Cart',
+            },
+
+
         ];
     }
 
     async componentDidMount() {
-        let user = await getUserDetails();       
-        this.setState({user: user});
+        let user = await getUserDetails();
+        this.setState({ user: user });
+        if(user.l_image ==1){
+            this.setState({ camerlogin: true });
+        }
+        if(user.l_finger ==1){
+            this.setState({ fig: true });
+        }
     }
 
     logoutUser = () => {
         logout();
         this.props.navigation.replace('Login');
-
-        // this.props.navigation.dispatch(
-        //     CommonActions.reset({
-        //         index: 1,
-        //         routes: [{name: 'Login'}],
-        //     }),
-        // );
     };
 
+
+    appshare = async() => {
+        const result = await Share.share({
+            message: 'Comming soon in play store',
+          });
+       
+    };
+
+    apprate = async() => {
+        const result = await Share.share({
+            message: 'Comming soon in play store',
+          });
+       
+    };
+
+    figLoginWithStatus = () =>{
+        if(this.state.fig == true){
+        this.setState({ fig: false });      
+         
+          this.changforfig()
+        }else{
+        this.setState({ fig: true });          
+         this.changforfig()    
+        }
+      }
+      camerloginWithStaut = () =>{
+        if(this.state.camerlogin == true){
+        this.setState({ camerlogin: false });         
+          this.changforcam()
+        }else{
+        this.setState({ camerlogin: true });          
+        this.changforcam()
+          
+        }
+      }
+
+      changforcam = () => {
+        if(this.state.camerlogin == true){
+            this.setState({check:1})      
+         }else{
+            this.setState({check:0})
+    
+         }
+           fetch(BASE_URL + "/login.php", {
+             method: 'POST',
+             body:JSON.stringify({
+               email:this.state.user.email,
+               type:"cal",
+               value:this.state.check
+             })
+             })
+             .then((response) => response.json())
+             .then((responseJson) => {
+            //    setIsLoaded(false);
+            //    showToastWithGravityAndOffset(responseJson.msg)
+             
+             })
+             .catch((error) => {
+               console.error(error);
+             });
+       
+         }
+
+    changforfig =  () =>{
+     if(this.state.fig == true){
+        this.setState({check:1})      
+     }else{
+        this.setState({check:0})
+
+     }
+        fetch(BASE_URL + "/login.php", {
+            method: 'POST',
+            body:JSON.stringify({
+              email:this.state.user.email,
+              type:"finlo",
+              value: this.state.check
+            })
+            })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson)
+             // setIsLoaded(false);
+             // showToastWithGravityAndOffset(responseJson.msg)
+            
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+    }
+
     getActiveRouteState = name => {
-    //     let active = false;
-    //     if (this.props.state !== undefined) {
-    //         let activeIndex = this.props.state.index;
-    //         let activeRouteName = this.props.state.routes[activeIndex].name;
-    //         if (activeRouteName === name) {
-    //             active = true;
-    //         }
-    //     }
-    //     return active;
+        let active = false;
+        if (this.props.state !== undefined) {
+            let activeIndex = this.props.state.index;
+            let activeRouteName = this.props.state.routes[activeIndex].name;
+            if (activeRouteName === name) {
+                active = true;
+            }
+        }
+        return active;
     };
 
     render() {
@@ -66,19 +170,26 @@ export default class CustomSidebarMenu extends Component {
             <View style={styles.sideMenuContainer}>
                 <ScrollView style={styles.scrollView}>
                     <View style={styles.profileContainer}>
-                        <Image
-                            source={require('../assets/images/user.png')}
-                            style={styles.sideMenuProfileIcon}
-                        />
+
+
+
+                        {this.state.user !== null ? (
+                            <Image
+                                source={{ uri: Base_Image + this.state.user.image }}
+                                style={styles.sideMenuProfileIcon}
+                            />
+                        ) : null}
+
+
                         {this.state.user !== null ? (
                             <View>
-                            <Text style={styles.title}>{this.state.user.name}</Text>
-                            <Text style={styles.email}>{this.state.user.email}</Text>
+                                <Text style={styles.title}>{this.state.user.name}</Text>
+                                <Text style={styles.email}>{this.state.user.email}</Text>
                             </View>
-                           
-                           
-
                         ) : null}
+
+
+
                     </View>
 
                     <View
@@ -89,9 +200,9 @@ export default class CustomSidebarMenu extends Component {
                         }}
                     />
 
-                    <View style={{width: '100%'}}>
+                    <View style={{ width: '100%' }}>
                         {this.items.map((item, key) => (
-                           
+
                             <TouchableOpacity
                                 onPress={() => {
                                     global.currentScreenIndex = key;
@@ -102,8 +213,9 @@ export default class CustomSidebarMenu extends Component {
                                     alignItems: 'center',
                                     paddingTop: 15,
                                     paddingBottom: 15,
-                                    marginTop: 10,
-                                    marginBottom:10,
+                                    marginTop: 0,
+                                    marginBottom: 0,
+                                   
                                     backgroundColor: this.getActiveRouteState(
                                         item.screenToNavigate,
                                     )
@@ -111,31 +223,133 @@ export default class CustomSidebarMenu extends Component {
                                         : "white",
                                 }}
                                 key={key}>
-                                <View style={{marginRight: 15, marginLeft: 20}}>
-                               
-                               
-                                  {/* <Icon
-                                        name={item.navOptionThumb}
-                                        size={22}
-                                        color={
-                                            this.getActiveRouteState(item.screenToNavigate)
-                                                ? 'red'
-                                                : '#808080'
-                                        }
-                                    /> */}
+                                <View style={{ marginRight: 15, marginLeft: 20 }}>
+
+                                    {item.navOptionThumb}
+
                                 </View>
                                 <Text
                                     style={{
                                         fontSize: 15,
-                                        // color: this.getActiveRouteState(item.screenToNavigate)
-                                        //     ? Color.colorPrimary
-                                        //     : 'black',
+                                        color: this.getActiveRouteState(item.screenToNavigate)
+                                            ? 'black'
+                                            : 'black',
                                     }}
                                 >
                                     {item.navOptionName}
                                 </Text>
                             </TouchableOpacity>
                         ))}
+
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                paddingTop: 10,
+                                paddingBottom: 0,
+                            }}>
+                            <View style={{ marginRight: 15, marginLeft: 20 }}>
+                            <Ionicons name="finger-print" size={24} color="black" />
+                            </View>
+                            <Text
+                                style={{
+                                    fontSize: 15,
+                                    color: 'black',
+                                }}
+                                >
+                                Finger
+                            </Text>
+                            <ToggleSwitch style={{ marginBottom: 20,marginLeft:'40%' }}                          
+                                isOn={this.state.fig}
+                                onColor="green"
+                                offColor="red"
+                                size="small"
+                                onToggle={() => {
+                                    this.figLoginWithStatus();
+                                }}
+                            />
+
+                        </View>
+
+                     <View
+                        style={{
+                            flexDirection: 'row',
+                            paddingTop: 10,
+                            paddingBottom: 10,
+                        }}>
+                        <View style={{ marginRight: 15, marginLeft: 20 }}>
+                        <MaterialCommunityIcons name="image-filter-center-focus" size={24} color="black" />
+                        </View>
+                        <Text
+                            style={{
+                                fontSize: 15,
+                                color: 'black',
+                            }}
+                           >
+                            Image
+                        </Text>
+                        <ToggleSwitch style={{ marginBottom: 20,marginLeft:'40%' }}
+                            isOn={this.state.camerlogin}
+                            onColor="green"
+                            offColor="red"                           
+                            size="small"
+                            onToggle={() => {
+                                this.camerloginWithStaut();
+                            }}
+
+                        />
+
+                        </View> 
+                        
+
+                        <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            paddingTop: 10,
+                            paddingBottom: 10,
+                        }}>
+                        <View style={{ marginRight: 15, marginLeft: 20 }}>
+									<MaterialIcons name="share" size={24} color="black" />
+                           
+                        </View>
+                        <Text
+                            style={{
+                                fontSize: 15,
+                                color: 'black',
+                            }}
+                            onPress={() => {
+                                this.appshare();
+                            }}
+                            >
+                            App Share
+                        </Text>
+                    </View>
+
+                    <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingTop: 10,
+                        paddingBottom: 10,
+                    }}>
+                    <View style={{ marginRight: 15, marginLeft: 20 }}>
+                                <MaterialIcons name="star" size={24} color="black" />
+                       
+                    </View>
+                    <Text
+                        style={{
+                            fontSize: 15,
+                            color: 'black',
+                        }}
+                        onPress={() => {
+                            this.apprate();
+                        }}
+                        >
+                        App Rate
+                    </Text>
+                </View>
+
+
                         <View
                             style={{
                                 flexDirection: 'row',
@@ -143,8 +357,8 @@ export default class CustomSidebarMenu extends Component {
                                 paddingTop: 10,
                                 paddingBottom: 10,
                             }}>
-                            <View style={{marginRight: 15, marginLeft: 20}}>                        
-                              <AntDesign name="logout" size={24} color="black" />
+                            <View style={{ marginRight: 15, marginLeft: 20 }}>
+                                <AntDesign name="logout" size={24} color="black" />
                             </View>
                             <Text
                                 style={{
@@ -180,7 +394,7 @@ const styles = StyleSheet.create({
         resizeMode: 'center',
         width: 50,
         height: 50,
-        borderRadius: 50 / 2,
+        borderRadius: 50,
     },
     title: {
         // fontFamily: Font.primarySemiBold,
@@ -188,10 +402,10 @@ const styles = StyleSheet.create({
         fontSize: 16,
         marginLeft: 10,
     },
-    email:{
-        fontSize:10,
-        marginLeft:10,
-        color:"grey"
+    email: {
+        fontSize: 10,
+        marginLeft: 10,
+        color: "grey"
     },
 
     profileContainer: {
